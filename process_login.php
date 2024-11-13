ee74535bae8557132895e5c55bdbb7a98fbbc167
Khant Thu Aung <wannathu9@gmail.com>
2024-11-13 16:12:35 +0800

    Added: Condition for error

--- process_login.php
+++ process_login.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 if (isset($_POST['login'])) {
     session_start();
@@ -6,18 +14,21 @@ if (isset($_POST['login'])) {
     $con = @mysqli_connect($host, $user, $pwd, $sql_db);
     $username = mysqli_real_escape_string($con, $_POST['user']);
     $password = mysqli_real_escape_string($con, $_POST['pass']);
+    
 
     $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $q2 = "UPDATE users set last_attempt = now() where username='$username'";
     $result = mysqli_query($con, $query);
-    $row = mysqli_fetch_assoc($result);
-
-    if ($row) {
+    mysqli_query($con, $q2);
+    if ($result and mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
         header('Location: manage.php');
-        exit;
     } else {
         echo '<p class="error-message">Invalid Username or Password.</p>';
         echo '<p class="back-to-home">Not an HR Manager? <a href="index.php">Go back to Home</a></p>';
     }
 }
 ?>
+</body>
+</html>
