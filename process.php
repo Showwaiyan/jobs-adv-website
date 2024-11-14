ca35f702ec2e6570ce1db653455e8172d3633840
Khant Thu Aung <wannathu9@gmail.com>
2024-11-14 12:10:28 +0800

    temporary table output

--- process.php
+++ process.php
@@ -1,49 +1,29 @@
 <?php 
-
-
 function show_result($result){
-    
-    $query = "SELECT * FROM skills WHERE EOInumber = '" . 12 . "'";
-    $result_skill = mysqli_query($conn, $query);
-    echo $query;
-   
-}
-function show_skill($eoinumber) {
-    require_once('settings.php');
-    $skills =[];
-    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-    $output = "";
-    // Check the connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){ 
+        echo "<tr>
+                    <td>". $row['EOInumber']."</td>
+                    <td>". $row['JRN']."</td>
+                    <td>". $row['fname']."</td>
+                    <td>". $row['lname']."</td>
+                    <td>". $row['dob']."</td>
+                    <td>". $row['gender']."</td>
+                    <td>". $row['streetaddress']."</td>
+                    <td>". $row['suburb']."</td>
+                    <td>". $row['state']."</td>
+                    <td>". $row['postcode']."</td>
+                    <td>". $row['phnumber']."</td>
+                    <td>". $row['otherskills']."</td>
+                    <td>". $row['Status']."</td>  
+                </tr>";
+    }
     }else{
-
-    // Prepare and execute the query
-            $query = "SELECT * FROM skills WHERE EOInumber = '" . $eoinumber . "'";
-            $result_skill = mysqli_query($conn, $query);
-            echo $query;
-            if ($result_skill) {
-                
-            while ($row = mysqli_fetch_assoc($result_skill)) {
-                for($i = 0; $i < mysqli_num_rows($result_skill); $i++) {
-                    $column_name = "Skill".($i+1);
-                    $skills[] = $row[$column_name];
-                    //
-                    // Display skills as an unordered list
-                    if (!empty($skills)) {
-                        foreach ($skills as $skill) {
-                            $output .= "<li>" . $skill . "</li>";
-                        }
-                        
-                    }
-                }
-            }
-    } else {
-        $output = "No skills found";
+        echo "<td>No records found</td>";
     }
 }
-    // Close the connection
-    mysqli_close($conn);
-    return $output;
+
+function show_jobs($result){
+    
 }
 ?>
