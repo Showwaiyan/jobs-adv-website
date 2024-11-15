f4acfe1da3fb6cf9e0ee75f2c09923dfe5807c1f
Show Wai Yan <showwaiyan555@gmail.com>
2024-11-15 21:36:12 +0800

    Added Job Edit Feature

--- manage.php
+++ manage.php
@@ -1,7 +1,7 @@
-<?php include_once "auth.php";
-require_once "settings.php";
-require_once 'process.php';
-$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+<?php #include_once "auth.php";
+#require_once "settings.php";
+#require_once 'process.php';
+#$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
  
 ?>
 <!DOCTYPE html>
@@ -29,6 +29,7 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
                         <option value="list_applicant" <?php if ($selectedAction == 'list_applicant') echo 'selected' ?>>List EOIs by Applicant Name</option>
                         <option value="delete_position" <?php if ($selectedAction == 'delete_position') echo 'selected' ?>>Delete EOIs by Job Reference</option>
                         <option value="change_status" <?php if ($selectedAction == 'change_status') echo 'selected' ?>>Change Status of EOIs</option>
+                        <option value="edit_job" <?php if ($selectedAction == 'edit_job') echo 'selected' ?>>Edit Job Position</option>
                     </select>
                 </div>
                 <button type="submit" name="itemSelected">Select Action</button>
@@ -83,12 +84,22 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
                 </select>
                 <button type="submit" name="statueChange">Change Status</button>
             </form>
-        <?php } ?>
-        
+        <?php } elseif ($selectedAction == 'edit_job') {?>
+            <form action="manage.php" method="POST">
+                <h2>Edit Details by Reference Number</h2>
+                <input type="hidden" name="action" value="edit_job">
+                <label for="edit_job">Job Reference Number:</label>
+                <input type="text" id="edit_job" name="edit_job" placeholder="Enter Reference Number" required>
+                <button type="submit" name="editJob">Edit</button>
+            </form> 
+        <?php }?>
         </section> 
 
         <section class="display-eoi">
         <table>
+
+                <?php if($conn){ ?>
+                <?php if($selectedAction != 'edit_job') {?>
                 <tr>
                     <th>EOInumber</th>
                     <th>Job Reference Number</th>
@@ -105,8 +116,13 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
                     <th>Other Skills</th>
                     <th>Status</th>
                 </tr>
-                <?php if($conn){ 
-                    if(isset($_POST['listAll']) || isset($_POST['itemSelected'])){
+                <?php } else {?>
+
+
+                    <!-- Job Table Header Goes Here -->
+
+
+                <?php } if(isset($_POST['listAll']) || isset($_POST['itemSelected'])){
                         $sql = "SELECT * FROM eoi";
                     }elseif(isset($_POST['position'])){
                         $job_reference = $_POST['job_reference'];
@@ -133,7 +149,74 @@ $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
                         show_result($all_list,$conn);
                         exit();
                     }
-                    $result = mysqli_query($conn, $sql);
+                    elseif(isset($_POST['editJob'])) { 
+                        $jobDes = []; ?>
+                        <form action="" method="POST">
+                        <fieldset class="job-description">
+                            <legend>Job Discription</legend>
+                            <p>
+                                <label for="jrn"> Job Reference Number: </label>
+                                <input type="text" id="jrn" name="jrn" maxlength="5" pattern="[A-Za-z0-9]{5}" value="<?php $jobDes['JobReferenceNumber'] ?>" required>
+                            </p>
+
+                            <p>
+                                <label for="jn">Job Title: </label>  
+                                <input type="text" id="jn" name="jn" maxlength="45" value="<?php $jobDes['JobTitle'] ?>" required>
+                            </p>
+
+                            <p>
+                                <label for="jt">Job Type: </label>
+                                <select name="jt" id="jt">
+                                    <option value="">Select Job Type</option>
+                                    <option value="ft" <?php if($jobDes['JobType'] == 'ft') echo 'selected' ?>>Full Time</option>
+                                    <option value="pt"<?php if($jobDes['JobType'] == 'pt') echo 'selected' ?>>Part Time</option>
+                                    <option value="it"<?php if($jobDes['JobType'] == 'it') echo 'selected' ?>>Intern</option>
+                                </select>
+                            </p>
+
+                            <p>
+                                <label for="loc">Location: </label>
+                                <input type="text" id="loc" name="loc" maxlength="45" value="<?php $jobDes['Location'] ?>" required>
+                            </p>
+
+                            <p>
+                                <label for="salary">Salary: </label>
+                                <input type="text" name="salary" id="salary" value="<?php $jobDes['salary'] ?>">
+                            </p>
+
+                            <p>
+                                <label for="company">Company: </label>
+                                <input type="text" name="company" id="company" value="<?php $jobDes['company'] ?>" required>
+                            </p>
+
+                            <p>
+                                <label for="reprot">Report To: </label>
+                                <input type="text" name="report" id="report" value="<?php $jobDes['report_to'] ?>" required>
+                            </p>
+                        </fieldset>
+
+                        < class="job-requirement">
+                            <?php $jobReq = []; ?>   
+                            <legend>Job Requirement</legend>
+                            <p>
+                                <label for="key_res">Key Responsibility</label>
+                                <textarea name="key_res" id="key_res" cols="30" rows="10" value="<?php $jobReq['key_responsibility'] ?>" required></textarea>
+                            </p>
+
+                            <p>
+                                <label for="essentials">Essentials</label>
+                                <textarea name="essentials" id="essentials" cols="30" rows="10" value="<?php $jobReq['essentials'] ?>" required></textarea>
+                            </p>
+
+                            <p>
+                                <label for="preferable">Preferable</label>
+                                <textarea name="preferable" id="preferable" cols="30" rows="10" value="<?php $jobReq['preferable'] ?>" ></textarea>
+                            </p>
+                        </fieldset>
+
+                        <button type="submit" name="editDone">Submit</button>
+                    </form>
+                    <?php } $result = mysqli_query($conn, $sql);
                     show_result($result,$conn);
                     ?>
                 <?php }else { ?>
