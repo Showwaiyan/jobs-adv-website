a4499e42dc78cab126adc7d75a59ab2bb381b167
Khant Thu Aung <wannathu9@gmail.com>
2024-11-16 11:39:17 +0800

    Added: Job Edit

--- process.php
+++ process.php
@@ -36,4 +36,77 @@ function show_skills($eoinumber,$conn){
     }
     return $output;
 }
+
+function show_jobs($result,$conn){
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){ 
+            $jrn = $row['JobReferenceNumber'];
+            $res = show_responsibility($jrn,$conn);
+            $ess = show_essentials($jrn,$conn);
+            $pref = show_preferable($jrn,$conn);
+        echo "
+                <tr>
+                    <td>". $row['JobReferenceNumber']."</td>
+                    <td>". $row['JobTitle']."</td>
+                    <td style='overflow-y:auto'>". $row['JobDescription']."</td>
+                    <td>". $row['JobType']."</td>
+                    <td>". $row['Location']."</td>
+                    <td>". $row['salary']."</td>
+                    <td>". $row['company']."</td>
+                    <td>". $row['report_to']."</td>
+                    <td>" . $res . "</td>
+                    <td>". $ess . "</td>
+                    <td>". $pref . "</td>
+                </tr>";
+    }
+    }else{
+        echo "<td>No records found</td>";
+    }
+}
+
+function show_responsibility($jrn,$conn){
+    $output =[];
+    $sql = "SELECT key_responsibility FROM job_requirements 
+                                    WHERE key_responsibility IS NOT NULL
+                                    AND key_responsibility <> ''
+                                    AND jrn= '$jrn'";
+                        // echo $respon ."<br>";;
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){ 
+            $output[] = $row['key_responsibility'];
+        }
+}
+return implode("\n", $output);
+}
+function show_essentials($jrn,$conn){
+    $output =[];
+    $sql = "SELECT essentials FROM job_requirements 
+                                    WHERE essentials IS NOT NULL
+                                    AND essentials <> ''
+                                    AND jrn= '$jrn'";
+                        // echo $respon ."<br>";;
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){ 
+            $output[] = $row['essentials'];
+        }
+}
+return implode("\n", $output);
+}
+function show_preferable($jrn, $conn){
+    $output =[];
+    $sql = "SELECT preferable FROM job_requirements 
+                                    WHERE preferable IS NOT NULL
+                                    AND preferable <> ''
+                                    AND jrn= '$jrn'";
+                        // echo $respon ."<br>";;
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)){ 
+            $output[] = $row['preferable'];
+        }
+}
+return implode("\n", $output);
+}
 ?>
