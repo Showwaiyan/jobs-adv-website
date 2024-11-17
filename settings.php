b9f97191086add0922c7c8b4802da52377096df3
Show Wai Yan <showwaiyan555@gmail.com>
2024-11-17 18:29:45 +0800

    Removed DB info

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php 
-    $host = "feenix-mariadb.swin.edu.au";
-    $user = "s105292912";
-    $pwd = "290501";
-    $sql_db = "s105292912_db";
+    $host = "";
+    $user = "";
+    $pwd = "";
+    $sql_db = "";
 ?>
